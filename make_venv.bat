ea4019df76d0b108a6ce54bd25d8f64a1759eff2
James Abel <j@abel.co>
2020-08-21 14:24:17 -0700

    initial dev

--- make_venv.bat
+++
@@ -1,5 +0,0 @@
-set PYTHON_EXE="\Program Files\Python38\python.exe"
-%PYTHON_EXE% -m venv --clear venv
-venv\Scripts\python.exe -m pip install --upgrade pip
-venv\Scripts\pip3 install -U setuptools
-venv\Scripts\pip3 install -U -r requirements-dev.txt
