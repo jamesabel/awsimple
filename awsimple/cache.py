ccbd2ab39f4dfa2e32ded36fb216b5b71a31ec7c
James Abel <j@abel.co>
2020-08-21 23:02:02 -0700

    lru cache dev

--- awsimple/cache.py
+++ awsimple/cache.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from shutil import disk_usage
 import os
 
+from typeguard import typechecked
 from balsa import get_logger
 
 from awsimple import __application_name__
@@ -9,12 +10,14 @@ from awsimple import __application_name__
 log = get_logger(__application_name__)
 
 
+@typechecked(always=True)
 def get_disk_free(path: Path = Path(".")) -> int:
     total, used, free = disk_usage(Path(path).absolute().anchor)
     log.info(f"{total=} {used=} {free=}")
     return free
 
 
+@typechecked(always=True)
 def get_directory_size(path: Path) -> int:
     size = 0
     for p in path.glob("*"):
@@ -25,5 +28,6 @@ def get_directory_size(path: Path) -> int:
     return size
 
 
+@typechecked(always=True)
 def remove_lru(cache_dir: Path, max_cache_size: int):
     raise NotImplementedError  # todo: finish this
