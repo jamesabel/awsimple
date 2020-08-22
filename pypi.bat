e1ee9631f61d8be7e5d5d0ad7b42aae5369aa429
James Abel <j@abel.co>
2020-08-21 21:16:11 -0700

    initial dev

--- pypi.bat
+++ pypi.bat
@@ -1,9 +1,7 @@
-del /Q yaaws.egg-info\*.*
+del /Q awsimple.egg-info\*.*
 del /Q build\*.*
 del /Q dist\*.*
 copy /Y LICENSE LICENSE.txt
-copy /Y docs\source\readme.rst readme.rst
-copy /Y docs\source\readme.rst sundry\readme.rst
 call venv\Scripts\activate.bat
 python.exe setup.py bdist_wheel
 twine upload dist/*
