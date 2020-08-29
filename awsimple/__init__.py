4bc12bd7140682e05b64edfad9eeae22ad0c12e7
James Abel <j@abel.co>
2020-08-28 20:40:25 -0700

    expose AWSS3ObjectMetadata

--- awsimple/__init__.py
+++ awsimple/__init__.py
@@ -2,4 +2,4 @@ from .__version__ import __application_name__, __version__, __author__, __title_
 from .aws import AWSAccess
 from .cache import get_disk_free, get_directory_size, lru_cache_write
 from .dynamodb import DynamoDBAccess, dict_to_dynamodb
-from .s3 import S3Access, AWSS3DownloadStatus
+from .s3 import S3Access, AWSS3DownloadStatus, AWSS3ObjectMetadata
