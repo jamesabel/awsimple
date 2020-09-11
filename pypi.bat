6176ad52a6013148e7844b1576ac892e5c618f8d
James Abel <j@abel.co>
2020-09-10 18:46:23 -0700

    fix rmdirs

--- pypi.bat
+++ pypi.bat
@@ -1,10 +1,10 @@
-del /Q awsimple.egg-info\*.*
-del /Q build\*.*
-del /Q dist\*.*
+rmdir /S /Q awsimple.egg-info
+rmdir /S /Q build
+rmdir /S /Q dist
 copy /Y LICENSE LICENSE.txt
 call venv\Scripts\activate.bat
 python.exe setup.py bdist_wheel
 twine upload dist/*
-del /Q awsimple.egg-info\*.*
-del /Q build\*.*
+rmdir /S /Q awsimple.egg-info
+rmdir /S /Q build
 deactivate
