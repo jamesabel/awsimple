ad4a9392726a24d79c2d216328231f8437d86fc7
gh-ppolk <ppolk@guardanthealth.com>
2021-03-01 20:35:23 -0800

    remove conflicting files to enable merge

--- examples/make_venv.sh
+++
@@ -1,5 +0,0 @@
-rm -rf venv
-python3.8 -m venv --clear venv
-./venv/bin/python -m pip install --upgrade pip
-./venv/bin/pip3 install -U setuptools
-./venv/bin/pip3 install -U -r requirements-examples.txt
--- examples/run_examples.sh
+++
@@ -1,5 +0,0 @@
-./venv/bin/activate
-python -m aws_access_test
-python -m write_read_s3_object
-python -m derived_access_class
-deactivate
