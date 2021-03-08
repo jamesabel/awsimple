5142c6294f0f5e3f6734ce8deb3c69812e161861
James Abel <j@abel.co>
2021-03-07 17:40:32 -0800

    sort key as an int

--- examples/dynamodb_partition_and_sort.py
+++ examples/dynamodb_partition_and_sort.py
@@ -13,22 +13,21 @@ def musical_instruments_example():
 
     """
 
-    dynamodb_access = DynamoDBAccess("musical_instruments_example", profile_name="testawsimple")
+    dynamodb_access = DynamoDBAccess("musical_instruments_example", profile_name="testawsimple", cache_life=60)  # short cache life for development
 
-    # our primary key is a composite of partition (manufacturer) and sort (serial_number).
-    # for a particular manufacturer, serial numbers define exactly one instrument
-    dynamodb_access.create_table("manufacturer", "serial_number")
+    # Our primary key is a composite of partition (manufacturer) and sort (serial_number).
+    # For a particular manufacturer, serial numbers define exactly one instrument (for this example we are assuming a serial number can be represented as an
+    # integer and doesn't have to be a string).
+    dynamodb_access.create_table("manufacturer", "serial_number", sort_key_type=int)
 
     # we have to convert float to a Decimal for DynamoDB
-    dynamodb_access.put_item(dict_to_dynamodb({"manufacturer": "Gibson", "serial_number": "1234", "model": "Ripper", "year": 1983, "price": 1250.0}))
-
-    dynamodb_access.put_item(dict_to_dynamodb({"manufacturer": "Gibson", "serial_number": "5678", "model": "Thunderbird", "year": 1977, "price": 2400.0}))
-
+    dynamodb_access.put_item(dict_to_dynamodb({"manufacturer": "Gibson", "serial_number": 1234, "model": "Ripper", "year": 1983, "price": 1299.50}))
+    dynamodb_access.put_item(dict_to_dynamodb({"manufacturer": "Gibson", "serial_number": 5678, "model": "Thunderbird", "year": 1977, "price": 2399.50}))
     dynamodb_access.put_item(
         dict_to_dynamodb(
             {
                 "manufacturer": "Fender",
-                "serial_number": "1234",
+                "serial_number": 1234,
                 "model": "Precision",
                 "year": 2008,
                 "price": 1800.0,
