e1ebc73cfbcb02a7bc166dd8575be0c83a904bac
James Abel <j@abel.co>
2021-03-15 21:01:03 -0700

    create AWS credentials and config on the fly for CI mocked testing

--- test_awsimple/conftest.py
+++ test_awsimple/conftest.py
@@ -8,10 +8,26 @@ from balsa import Balsa
 
 from awsimple import __application_name__, __author__, is_mock, use_moto_mock_env_var
 
+from test_awsimple import test_awsimple_str
+
 mock_env_var = os.environ.get(use_moto_mock_env_var)
 
 if mock_env_var is None:
     os.environ[use_moto_mock_env_var] = "1"
+
+# if using non-local pytest, create the credentials and config files dynamically
+aws_credentials_and_config_dir = Path(Path.home(), ".aws")
+aws_credentials_file = Path(aws_credentials_and_config_dir, "credentials")
+aws_config_file = Path(aws_credentials_and_config_dir, "config")
+if is_mock():
+    if not aws_credentials_file.exists():
+        credential_strings = ["[default]\naws_access_key_id=AAAAAAAAAAAAAAAAAAAA\naws_secret_access_key=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+                              f"[{test_awsimple_str}]\naws_access_key_id=AAAAAAAAAAAAAAAAAAAA\naws_secret_access_key=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"]
+        aws_credentials_file.write_text("\n".join(credential_strings))
+    if not aws_config_file.exists():
+        config_strings = ["[profile default]\nregion=us-west-2",
+                          f"[profile {test_awsimple_str}]\nregion=us-west-2"]
+        aws_config_file.write_text("\n".join(config_strings))
 
 
 class TestAWSimpleLoggingHandler(logging.Handler):
