a353807cbd085aaedde773eac5ee8b5ac1a2a76c
James Abel <j@abel.co>
2021-03-15 18:53:01 -0700

    add codecov

--- scripts/pytest.bat
+++ scripts/pytest.bat
@@ -4,7 +4,7 @@ cd ..
 call venv\Scripts\activate.bat
 set PYTHONPATH=%CD%
 set AWSIMPLE_USE_MOTO_MOCK=1
-python -m pytest
+python -m pytest -s test_awsimple --cov-report xml:coverage.xml --cov-report html --cov=.\awsimple
 set AWSIMPLE_USE_MOTO_MOCK=0
 python -m pytest
 set PYTHONPATH=
