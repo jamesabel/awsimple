c913ca67dcd6e55d625baa4923811b8ff1b8887a
James Abel <j@abel.co>
2021-11-14 08:40:24 -0800

    blackify

--- test_awsimple/test_s3_object_floats.py
+++ test_awsimple/test_s3_object_floats.py
@@ -1,7 +1,5 @@
-
-
 def test_s3_object_floats(s3_access):
-    object_with_floats = {"0.1": 2.3456789E-11}
+    object_with_floats = {"0.1": 2.3456789e-11}
     s3_key = "a"
     s3_access.upload_object_as_json(object_with_floats, s3_key)
     s3_object = s3_access.download_object_as_json_cached(s3_key)
