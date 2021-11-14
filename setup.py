781053598401efc6050dabd933a91db96e5bf152
James Abel <j@abel.co>
2021-11-14 08:38:57 -0800

    minimum hashy version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     keywords=["aws", "cloud", "storage", "database", "dynamodb", "s3"],
     packages=[__title__],
     package_data={__title__: [readme_file_path, "py.typed"]},
-    install_requires=["boto3", "typeguard", "hashy", "dictim", "appdirs", "tobool"],
+    install_requires=["boto3", "typeguard", "hashy>=0.1.1", "dictim", "appdirs", "tobool"],
     project_urls={"Documentation": "https://awsimple.readthedocs.io/"},
     classifiers=[],
 )
