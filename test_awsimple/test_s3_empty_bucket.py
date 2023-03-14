0d644403699cefad84035788e1786c0982d9ade2
JamesAbel <j@abel.co>
2023-03-14 16:53:41 -0700

    bucket name all lower case

--- test_awsimple/test_s3_empty_bucket.py
+++ test_awsimple/test_s3_empty_bucket.py
@@ -1,3 +1,4 @@
+import time
 import platform
 import getpass
 
@@ -5,8 +6,10 @@ from awsimple import S3Access
 
 
 def test_s3_empty_bucket():
-    bucket_name = f"emptybuckettest{platform.node()}{getpass.getuser()}"  # must be globally unique when using real S3
+    bucket_name = f"emptybuckettest{platform.node()}{getpass.getuser()}".lower()  # must be globally unique when using real S3
     print(f"{bucket_name=}")
     s3_access = S3Access(bucket_name)
     s3_access.create_bucket()
+    assert s3_access.bucket_exists()
     assert len(s3_access.dir()) == 0
+    s3_access.delete_bucket()
