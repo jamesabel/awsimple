daa6ed2ff86a364dcf3ce9eddd7949287d625098
JamesAbel <j@abel.co>
2023-04-09 20:05:17 -0700

    add TableNotFound exception

--- test_awsimple/test_dynamodb_table_not_found.py
+++ test_awsimple/test_dynamodb_table_not_found.py
@@ -6,7 +6,6 @@ from test_awsimple import test_awsimple_str
 
 
 def test_dynamodb_table_not_found():
-
     with pytest.raises(DynamoDBTableNotFound):
         dynamodb_access = DynamoDBAccess("does_not_exist", profile_name=test_awsimple_str)
         dynamodb_access.upsert_item(item={})  # table won't exist
