965c0e1bf6c6ff244dc729b4eeb1c597fb59ec5d
JamesAbel <j@abel.co>
2024-03-05 16:01:52 -0800

    drop get_account_id()

--- test_awsimple/test_get_account_id.py
+++ test_awsimple/test_get_account_id.py
@@ -1,11 +1,13 @@
+import pytest
+
 from ismain import is_main
 
-from awsimple import AWSAccess, is_mock
+from awsimple import AWSAccess
 
 
 def test_get_account_id():
-    if not is_mock():
-        # todo: get this to work with mocking
+
+    with pytest.raises(NotImplementedError):
         aws_access = AWSAccess()
         account_id = aws_access.get_account_id()
         assert len(account_id) >= 12  # currently all account IDs are 12 numeric digits, but allow for them to increase in size (but still be only digits)
