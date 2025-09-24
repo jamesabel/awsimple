c8f01b36e6789bae507b16b157e76f982d8b7856
JamesAbel <j@abel.co>
2025-09-23 21:27:57 -0700

    add pubsub

--- awsimple/__init__.py
+++ awsimple/__init__.py
@@ -6,6 +6,7 @@ from .dynamodb import DynamoDBAccess, dict_to_dynamodb, DBItemNotFound, DynamoDB
 from .dynamodb import KeyType, aws_name_to_key_type
 from .dynamodb_miv import DynamoDBMIVUI, miv_string, get_time_us, miv_us_to_timestamp
 from .s3 import S3Access, S3DownloadStatus, S3ObjectMetadata, BucketNotFound
-from .sqs import SQSAccess, SQSPollAccess, aws_sqs_long_poll_max_wait_time, aws_sqs_max_messages
+from .sqs import SQSAccess, SQSPollAccess, aws_sqs_long_poll_max_wait_time, aws_sqs_max_messages, get_all_sqs_queues
 from .sns import SNSAccess
+from .pubsub import PubSub
 from .logs import LogsAccess
---
+++ awsimple/pubsub.py
@@ -0,0 +1,192 @@
+"""
+pub/sub abstraction on top of AWS SNS and SQS using boto3.
+"""
+import time
+from typing import Any, Dict, List
+from datetime import timedelta
+from multiprocessing import Process, Queue
+from multiprocessing import Event as MultiprocessingEvent
+from threading import Thread
+from queue import Empty
+from logging import Logger
+import json
+
+from typeguard import typechecked
+from botocore.exceptions import ClientError
+
+from .sns import SNSAccess
+from .sqs import SQSPollAccess, get_all_sqs_queues
+from .dynamodb import _DynamoDBMetadataTable
+from .platform import get_node_name
+from .__version__ import __application_name__
+
+log = Logger(__application_name__)
+
+queue_timeout = timedelta(days=30).total_seconds()
+
+def remove_old_queues() -> None:
+    """
+    Remove old SQS queues that have not been used recently.
+    """
+    for sqs_queue_name in get_all_sqs_queues():
+        sqs_metadata = _DynamoDBMetadataTable(sqs_queue_name)
+        mtime = sqs_metadata.get_table_mtime_f()
+        if mtime is not None and time.time() - mtime > queue_timeout:
+            log.info(f'deleting "{sqs_queue_name}",{mtime=}')
+            sqs = SQSPollAccess(sqs_queue_name)
+            try:
+                sqs.delete_queue()
+            except ClientError:
+                pass  # already doesn't exist
+
+@typechecked()
+def _connect_sns_to_sqs(channel_name: str, sqs_queue_name: str, sqs: SQSPollAccess) -> None:
+    """
+    Connect an SQS queue to an SNS topic.
+
+    :param channel_name: SNS topic name.
+    :param sqs_queue_name: SQS queue name.
+    :param sqs: SQSPollAccess instance for the SQS queue.
+    :return: None
+    """
+
+    sqs_arn = sqs.get_arn()
+
+    # Find the topic by name
+    sns = SNSAccess(channel_name)
+    sns.create_topic()
+    topic_arn = sns.get_arn()
+    topic = sns.resource.Topic(topic_arn)
+
+    # Subscribe queue to topic
+    queue_arn = sqs.get_arn()
+    subscription = topic.subscribe(Protocol="sqs", Endpoint=queue_arn)
+    log.info(f"Subscribed {sqs_queue_name} to topic {topic_arn}. Subscription ARN: {subscription.arn}")
+
+    # Update queue policy to allow SNS -> SQS
+    policy = {
+        "Version": "2012-10-17",
+        "Id": "sns-sqs-subscription-policy",
+        "Statement": [
+            {
+                "Sid": "Allow-SNS-SendMessage",
+                "Effect": "Allow",
+                "Principal": {"Service": "sns.amazonaws.com"},
+                "Action": "sqs:SendMessage",
+                "Resource": sqs_arn,
+                "Condition": {"ArnEquals": {"aws:SourceArn": topic_arn}},
+            }
+        ],
+    }
+    sqs.queue.set_attributes(Attributes={"Policy": json.dumps(policy)})
+    log.debug(f"Queue {sqs_queue_name} policy updated to allow topic {topic_arn}.")
+
+class _SubscriptionThread(Thread):
+
+    """
+    Thread to poll SQS for new messages and put them in a queue for the parent process to read.
+    """
+
+    @typechecked()
+    def __init__(self, sqs: SQSPollAccess, new_event: MultiprocessingEvent) -> None:
+        super().__init__()
+        self._sqs = sqs
+        self.new_event = new_event
+        self.sub_queue = Queue()
+
+    def run(self):
+        # exit by terminating the parent process
+        while True:
+            messages = self._sqs.receive_messages()  # long poll
+            for message in messages:
+                message = json.loads(message.message)
+                self.sub_queue.put(message["Message"])
+                self.new_event.set()  # notify parent process that a new message is available
+
+class PubSub(Process):
+
+    @typechecked()
+    def __init__(self, channel: str, node_name: str = get_node_name()) -> None:
+        """
+        Pub and Sub.
+        Create in a separate process to offload from main thread. Also facilitates use of moto mock in tests.
+
+        :param channel: Channel name (SNS topic name).
+        :param node_name: Node name (SQS queue name suffix). Defaults to a combination of computer name and username, but can be passed in for customization and/or testing.
+        """
+        self.channel = channel
+        self.node_name = node_name  # e.g., computer name
+
+        self._pub_queue = Queue()
+        self._sub_queue = Queue()
+
+        self._new_event = MultiprocessingEvent()  # pub or sub sets this when a new message is available or has been sent
+
+        super().__init__()
+
+    def run(self):
+
+        sqs_queue_name = f"{self.channel}_{self.node_name}"
+
+        sns = SNSAccess(self.channel, auto_create=True)
+        sqs_metadata = _DynamoDBMetadataTable(sqs_queue_name)
+
+        sqs = SQSPollAccess(sqs_queue_name)
+        if not sqs.exists():
+            sqs.create_queue()
+            _connect_sns_to_sqs(self.channel, sqs_queue_name, sqs)
+
+        sqs_metadata.update_table_mtime()  # update SQS use time (the existing infrastructure calls it a "table", but we're using it for the SQS queue)
+        remove_old_queues()  # clean up old queues
+
+        sqs_thread = _SubscriptionThread(sqs, self._new_event)
+        sqs_thread.start()
+
+        while True:
+
+            # pub
+            try:
+                message = self._pub_queue.get(False)
+                message_string = json.dumps(message)
+                sns.publish(message_string)
+            except Empty:
+                pass
+
+            # sub
+            try:
+                message = sqs_thread.sub_queue.get(False)
+                self._sub_queue.put(message)
+            except Empty:
+                pass
+
+            # this helps responsiveness
+            self._new_event.clear()
+            self._new_event.wait(3)  # race conditions can occur, so don't make this wait timeout too long
+
+    @typechecked()
+    def publish(self, message: dict) -> None:
+        """
+        Publish a message.
+
+        :param message: message as a dictionary
+        """
+        self._pub_queue.put(message)
+        self._new_event.set()
+
+    @typechecked()
+    def get_messages(self) -> List[Dict[str, Any]]:
+        """
+        Get all available messages.
+
+        :return: list of messages as dictionaries
+        """
+        messages = []
+        while True:
+            try:
+                message_string = self._sub_queue.get(block=False)
+                message = json.loads(message_string)
+                log.debug(f"{message=}")
+                messages.append(message)
+            except Empty:
+                break
+        return messages
