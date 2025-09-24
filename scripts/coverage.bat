ad3cf7716fe130442499cf4d796b414efb4d7375
James Abel <j@abel.co>
2025-09-24 01:16:39 -0700

    Cleanup (#23)
    * cleanup typing
    
    * cleanup typing
    
    * python 3.13 and 3.14
    
    * update CI for python pre-releases
    
    * use moto for coverage
    
    * attempt to get coverage when using multiprocessing library
    
    * Python 3.14
    
    * localstack not working for Python 3.14

--- scripts/coverage.bat
+++ scripts/coverage.bat
@@ -1,7 +1,7 @@
 pushd .
 cd ..
 set PYTHONPATH=%CD%
-set AWSIMPLE_USE_MOTO_MOCK=0
+set AWSIMPLE_USE_MOTO_MOCK=1
 mkdir cov
 venv\Scripts\pytest.exe --cov-report=html --cov-report=xml:cov\coverage.xml --cov --ignore=examples
 venv\Scripts\python.exe scripts\doc_coverage_updater.py
