6513f020e70e56d1a4b8d3bb5bc1ab6278876fd3
JamesAbel <j@abel.co>
2026-01-11 20:10:08 -0800

    Python 3.14

--- examples/make_venv.bat
+++ examples/make_venv.bat
@@ -1,5 +1,5 @@
 rmdir /S /Q venv
-c:"\Program Files\Python39\python.exe" -m venv --clear venv
+c:"\Program Files\Python314\python.exe" -m venv --clear venv
 venv\Scripts\python.exe -m pip install --upgrade pip
 venv\Scripts\pip3 install -U setuptools
 venv\Scripts\pip3 install -U -r requirements-examples.txt
